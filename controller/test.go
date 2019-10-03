74ce842d446091c00f7d931f4f6f9d76daa78bed
wangsin <wang_sin@163.com>
2019-10-03 18:12:33 +0800

    bug fix && gin配置

--- controller/test.go
+++ controller/test.go
@@ -1,11 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func TestController(c *gin.Context) {
-	userAgent := c.GetHeader("user-agent")
+	host := viper.GetString("mysql_main.host")
+	database := viper.GetString("mysql_main.database")
+	user := viper.GetString("mysql_main.user")
+	password := viper.GetString("mysql_main.password")
 
 	c.JSON(200, gin.H{
-		"message": userAgent,
+		"user-agent": c.GetHeader("user-agent"),
+		"develop-environment": viper.GetString("dev.type"),
+		"mysql-connection-url": fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, database),
 	})
 }
--- router/router.go
+++ router/router.go
@@ -3,28 +3,18 @@ package router
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	"sinblog.cn/FunAnime-Server/controller"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
-	host := viper.GetString("mysql_main.host")
-	database := viper.GetString("mysql_main.database")
-	user := viper.GetString("mysql_main.user")
-	password := viper.GetString("mysql_main.password")
 
 	testGroup := r.Group("/v1/test")
 	testGroup.Use(func(c *gin.Context) {
 		fmt.Println("This Is Middleware Func 1")
 	})
 	{
-		testGroup.GET("/ping", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"user-agent": c.GetHeader("user-agent"),
-				"develop-environment": viper.GetString("dev.type"),
-				"mysql-connection-url": fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, password, host, database),
-			})
-		})
+		testGroup.GET("/ping", controller.TestController)
 	}
 
 	return r
